540e9f73078f34b098c090b248c7fce8662e9922
Olga Bogdanova <TomieForever@gmail.com>
2021-08-19 01:24:30 +0300

    fix: mistakes

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-const upBtn= document.querySelector('.up-button');
-const downBtn= document.querySelector('.up-button');
-const sidebar= document.querySelector('.sidebar');
-const mainSlide=document.querySelector('.main-slide');
-const slidesCount=mainSlide.querySelectorAll('div');
-const container=document.querySelector('.container');
+function main() {
+const sliderContainer = document.querySelector('.slider-container');
+const slideRight = document.querySelector('.right-slide');
+const slideLeft = document.querySelector('.left-slide');
 
-sidebar.style.top=`-${(slidesCount-1)*100}vh`;
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+
+const slidesLength = slideLeft.querySelectorAll('div').length;
+
 let activeSlideIndex=0;
-upBtn.addEventListener('click',()=>{
-    changeSlide(up);
-})
-downBtn.addEventListener('click',()=>{
-    changeSlide(down);
-})
+slideLeft.style.top = `-${(slidesLength-1)*100}vh`;
 
-function changeSlide() {
-if (direction==="up"){
-    activeSlideIndex++;
-    if (activeSlideIndex===slidesCount){
-        activeSlideIndex=0;
+upButton.addEventListener('click',()=>changeSlide('up'));
+downButton.addEventListener('click',()=>changeSlide('down'));
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight;
+    if(direction==='up'){
+        activeSlideIndex++;
+        if(activeSlideIndex>slidesLength-1){
+            activeSlideIndex= 0;
+        }
+    } else if(direction==='down') {
+        activeSlideIndex--;
+        if(activeSlideIndex < 0){
+            activeSlideIndex=slidesLength-1;
+        }
     }
+
+   slideRight.style.transform = `translateY(-${activeSlideIndex*sliderHeight}px)`;
+   slideLeft.style.transform = `translateY(${activeSlideIndex*sliderHeight}px)`;
 }
-else if (direction==="down"){
-    activeSlideIndex--;
-    if (activeSlideIndex<0){
-        activeSlideIndex=slidesCount-1;
-    }
-
 }
+window.addEventListener('load', function(event) {
+    main();
+  });
 
-const height=container.clientHeight;
-
-mainSlide.style.transform=`translateY
-(-${activeSlideIndex*height}px)`
-sidebar.style.transform=`translateY
-(${activeSlideIndex*height}px)`
-
-}
